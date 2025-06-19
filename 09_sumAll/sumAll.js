cead0ae93b1323b8de3d6aa16bfaf3a69e5c0bc8
Adityan0198 <152312999+Adityan0198@users.noreply.github.com>
2025-06-19 13:28:54 +0530

    09_sumAll Done (Without loops)

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (!(Number.isInteger(a) &&
+        Number.isInteger(b) &&
+        a>=0 &&
+        b>=0
+    )) {return "ERROR";}
+    let small, big;
+    if (a<b) {
+        small=a;
+        big=b
+    }
+    else {
+        big = a;
+        small = b;
+    }
+    const n = big-small;
+    return n*(n+1)/2 + (n+1)*small;
 };
 
 // Do not edit below this line
