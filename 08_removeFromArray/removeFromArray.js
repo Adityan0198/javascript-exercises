7e3e3c3dd654756699a3d12901d4ea260431b11a
Adityan0198 <152312999+Adityan0198@users.noreply.github.com>
2025-06-19 13:03:08 +0530

    08_removeFromArray Done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    return array.filter(elem => !args.includes(elem));
 };
 
 // Do not edit below this line
