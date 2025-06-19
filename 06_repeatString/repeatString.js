1342808910270f6ae8fee6d63e2d88675baaafea
Adityan0198 <152312999+Adityan0198@users.noreply.github.com>
2025-06-19 12:34:44 +0530

    Completed 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times<0) {return "ERROR";}
+    let repeatedStr = '';
+    for (let i=0; i<times; i++) {
+        repeatedStr+=string;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
