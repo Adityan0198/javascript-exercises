f9bfa34fb28cb97f8cacc0cbefe18b79b2a295a5
Adityan0198 <152312999+Adityan0198@users.noreply.github.com>
2025-06-22 20:55:19 +0530

    Completed 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n<0) {return "OOPS";}
+    let curr=0;
+    let next=1;
+    for (let i=0; i<n; i++){
+        const temp = curr+next;
+        curr = next;
+        next = temp;
+    }
+    return curr;
 };
 
 // Do not edit below this line
