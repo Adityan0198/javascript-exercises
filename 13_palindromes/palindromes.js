52ac8342644a9f1372a9081f7978ad02b5138f07
Adityan0198 <152312999+Adityan0198@users.noreply.github.com>
2025-06-22 20:44:56 +0530

    Completed 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = string.toUpperCase().split('');
+    letters = 'QWERTYUIOPASDFGHJKLZXCVBNM1234567890';
+    array = array.filter(char => letters.includes(char));
+    const halfSize = array.length/2;
+    for(let i = 0; i<=halfSize; i++){
+        if (array[i]!==array[array.length-i-1]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
