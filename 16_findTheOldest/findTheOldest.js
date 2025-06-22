9e6dc091ac84be1bb57ff6527597a257007c1df6
Adityan0198 <152312999+Adityan0198@users.noreply.github.com>
2025-06-22 21:21:13 +0530

    16_findTheOldest done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const age = (person) => {
+        const referYear = person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear();
+        return referYear - person.yearOfBirth;
+    };
+    return people.reduce((oldest, person)=>
+        age(person)>age(oldest) ? person : oldest
+    );
 };
 
 // Do not edit below this line
